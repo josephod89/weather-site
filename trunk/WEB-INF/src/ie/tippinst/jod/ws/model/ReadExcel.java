785ef9101c8f27b494078a5d89d22f80ac05e616
josephod89@gmail.com <josephod89@gmail.com@101ed74e-f3cb-c7f7-a3b9-598336aa72ea>
2011-02-11 17:57:25 +0000

    Reads temperatures for this day last year, computes the average temperature and displays it.

--- trunk/WEB-INF/src/ie/tippinst/jod/ws/model/ReadExcel.java
+++ trunk/WEB-INF/src/ie/tippinst/jod/ws/model/ReadExcel.java
@@ -2,52 +2,58 @@ package ie.tippinst.jod.ws.model;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 import jxl.Cell;
-import jxl.CellType;
 import jxl.Sheet;
 import jxl.Workbook;
 import jxl.read.biff.BiffException;
 
 public class ReadExcel {
 
-	private String inputFile;
-
-	public void setInputFile(String inputFile) {
-		this.inputFile = inputFile;
-	}
-
-	public String read() throws IOException  {
-		File inputWorkbook = new File(inputFile);
+	public Collection<Weather> read(String filename, Date date) throws IOException  {
+		File inputWorkbook = new File(filename);
 		Workbook w = null;
 		Sheet sheet = null;
 		Cell cell = null;
+		Calendar current = new GregorianCalendar();
+		current.setTime(date);
+		Date d = null;
+		Calendar cal = null;
+		Collection<Weather> col = new ArrayList<Weather>();
+		
 		try {
 			w = Workbook.getWorkbook(inputWorkbook);
-			// Get the first sheet
 			sheet = w.getSheet(0);
-			// Loop over first 10 column and lines
-
-			/*for (int j = 0; j < sheet.getColumns(); j++) {
-				for (int i = 0; i < sheet.getRows(); i++) {
-					Cell cell = sheet.getCell(j, i);
-					CellType type = cell.getType();
-					if (cell.getType() == CellType.LABEL) {
-						System.out.println("I got a label "
-								+ cell.getContents());
-					}
-
-					if (cell.getType() == CellType.NUMBER) {
-						System.out.println("I got a number "
-								+ cell.getContents());
-					}
-
+			
+			DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+			
+			for (int i = 2; i < sheet.getRows(); i++) {
+				cell = sheet.getCell(0, i);
+				d = df.parse(cell.getContents());
+				cal = new GregorianCalendar();
+				cal.setTime(d);
+				if(current.get(Calendar.DATE) == cal.get(Calendar.DATE) && current.get(Calendar.MONTH) == cal.get(Calendar.MONTH)){
+					cell = sheet.getCell(2, i);
+					Weather weather = new Weather();
+					weather.setDate(d);
+					weather.setTemperature(Double.parseDouble(cell.getContents()));
+					col.add(weather);
+					//break;
 				}
-			}*/
-			cell = sheet.getCell(2, 2);
+			}
 		} catch (BiffException e) {
 			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
 		}
-		return cell.getContents();
+		return col;
 	}
 }
